0c982ee618fa86c58d59d45400a30f441e4cb86b
Alex Teplov <teplov0@gmail.com>
2016-09-11 01:26:10 +0500

    some midnight

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,5 +3,13 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
+		#render plain: params[:article].inspect
+		@article = Article.new(params.require(:article).permit(:title, :text))
+		@article.save
+		redirect_to @article
+	end
+	private
+	def acrticle_params
+		params.require(:article).permit(:title, :text)
 	end
 end
