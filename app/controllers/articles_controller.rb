ab3eba47ad83e6edee15dd0c827b86d951999ce6
Alex Teplov <teplov0@gmail.com>
2016-09-12 14:29:37 +0500

    Change code style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,24 @@
 class ArticlesController < ApplicationController
-	def index
-		@articles = Article.all
-	end
-	
-	def show
-		@article = Article.find(params[:id])
-	end
-	
-	def new
-	end
-
-	def create
-		#render plain: params[:article].inspect
-		@article = Article.new(params.require(:article).permit(:title, :text))
-		
-		@article.save
-		redirect_to @article
-	end
-	private
-	def acrticle_params
-		params.require(:article).permit(:title, :text)
-	end
+#init index 
+  def index
+    @articles = Article.all
+  end
+#init show
+  def show
+    @article = Article.find(params[:id])
+  end
+#init new
+  def new; end
+#intit create
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(acrticle_params)
+    @article.save
+    redirect_to @article
+  end
+# section private
+  private
+  def acrticle_params
+    params.require(:article).permit(:title, :text)
+  end
 end
